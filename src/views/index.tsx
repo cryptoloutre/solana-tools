7f877220e02b0df61e6b63c1b80bd66db54340c1
cryptoloutre <miningcryptomonnaie@gmail.com>
2022-06-16 11:07:59 +0200

    fix build issue

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,6 +1,4 @@
 export { HomeView } from "./HomeView";
 export { GalleryView } from "./GalleryView";
 export { SPLTokenView } from "./SPLTokenView";
-export { EditTokenView } from "./EditTokenView";
-export { BurnSPLView } from "./BurnSPLView";
 export { UploadView } from "./UploadView"
