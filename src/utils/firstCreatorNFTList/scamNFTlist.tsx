f0f7cfc3c4a73efbd9296dc7c83550d2035b27cf
cryptoloutre <miningcryptomonnaie@gmail.com>
2022-07-13 20:34:08 +0200

    update scam NFT list

--- src/utils/firstCreatorNFTList/scamNFTlist.tsx
+++ src/utils/firstCreatorNFTList/scamNFTlist.tsx
@@ -1,5 +1,20 @@
 export const scamNFTlist = [
     'XzR7CUMqhDBzbAm4aUNvwhVCxjWGn1KEvqTp3Y8fFCD',
-    'BN2BAwEEBjHoi4JnyKJ1aBfeq7qHxWksHpknRQeditd3'
+    'BN2BAwEEBjHoi4JnyKJ1aBfeq7qHxWksHpknRQeditd3',
+    'CfQA65wd8po9rFbpeR2uJHGqkPwpR4NSA6XQxxGvRNgA',
+    'CSE6pyuMZn1JtdZbtDg6wo7tXJJPKutWezfkon97c3NS',
+    '6kTErzxDR7uMjqjbh3cRSyCmd2YxefxHFsmaPURHUcqT',
+    'HREk65KiSb8eQJkQx9WYT8dEwzBBfN5sDcg8nQPFcTsN',
+    'EYwnq7q276WVZYpBwPapHGP5DxHJyRr77nGbTQAhLJSi',
+    'HvVpUyyUDX55FbfECQKTJZwprKoCRg8xbhjbqEetbUzh',
+    '3DYHjCxSFnmnHR4JDsL39cC5QPSLijHqDJYksE7dRBvD',
+    'AefDuhbkBHtK8AcineKxtEUstkA119CKXH2hcPCpzK8s',
+    '6AkraVhe7K4cL3gcPNFmNJgg4UZvQWpJVVKj3TPMcHwv',
+    'AdpBNUMJ8HSnUd85QgEYRmzBjim9n1K9LhCNBukNppuT',
+    '5AatLCbXP8cektqUmTcxqYrjfy9CU3t7B3U1RxNNmfdE',
+    '6srnATeMhYhDeBz3NAgrjiVbRuMCx9WyHJ6KGXJdnaDZ',
+    'FqxnP38R1TjtwBqp3jp8h7Yzk2WwRzFgMNMTAbwrA1uK',
+    'B9ZAqt8wefjGGTf3ENuUUXF1YKvH9TYTXiyoWkq9Pkwj',
+    'EqrQLEHVe7fkNiTZemQagQBA6qi6yX8yDvP46JgRpoip'
 
 ]
