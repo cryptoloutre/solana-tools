12410d99741ff6eed637b452af1460c9210644fa
cryptoloutre <miningcryptomonnaie@gmail.com>
2022-08-07 14:48:10 +0200

    add bonfida domain sending

--- src/views/MultiSenderView/index.tsx
+++ src/views/MultiSenderView/index.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { FC, useEffect, useState } from "react";
+import { FC, useState } from "react";
 import { useWallet, useConnection } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 
@@ -8,7 +8,7 @@ import styles from "./index.module.css";
 
 import { ASSOCIATED_TOKEN_PROGRAM_ID, Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { PublicKey, Transaction, TransactionInstruction, LAMPORTS_PER_SOL, SystemProgram, Connection } from '@solana/web3.js';
-import { getHashedName, getNameAccountKey, getTwitterRegistry, NameRegistryState } from "@bonfida/spl-name-service";
+import { getDomainKey, getHashedName, getNameAccountKey, getTwitterRegistry, NameRegistryState, transferNameOwnership } from "@bonfida/spl-name-service";
 
 const walletPublicKey = "";
 
@@ -189,6 +189,31 @@ export const MultiSenderView: FC = ({ }) => {
 
 
   }
+
+  // allow to check if the user owns the solana domain it wants to transfer
+  const checkDomainOwnership = async (Domains: string[]) => {
+
+
+    for (let i = 0; i < Domains.length; i++) {
+      const domainName = Domains[i].replace(".sol", "")
+
+      // get the key of the domain name
+      const { pubkey } = await getDomainKey(domainName);
+
+      // get the owner 
+      const owner = await NameRegistryState.retrieve(
+        connection,
+        pubkey
+      );
+      const ownerAddress = owner.registry.owner.toBase58();
+      if (ownerAddress != publicKey?.toBase58()) {
+        setError('You are not the owner of ' + Domains[i])
+        return false
+      }
+    }
+    return true
+  }
+
 
 
   // allow to multi send one token to multiple wallets
@@ -604,6 +629,95 @@ export const MultiSenderView: FC = ({ }) => {
     }
   }
 
+  const SendOnClickDomain = async () => {
+    if (publicKey) {
+      setError('');
+      setSignature('');
+
+      // init temp lists in order to clean them and remove the inputs with no value
+      const _Domains = [token1, token2, token3, token4, token5, token6, token7, token8, token9, token10];
+      const Domains: string[] = [];
+
+      for (let i = 0; i < _Domains.length; i++) {
+        if (_Domains[i] != '') {
+          Domains.push(_Domains[i]);
+
+        }
+      };
+      const isOwner = await checkDomainOwnership(Domains);
+
+      if (isOwner) {
+
+        try {
+          setIsSending(true)
+
+          let destPubkey: PublicKey;
+
+          // check if it is a SOL domain name
+          if (ReceiverAddress.includes('.sol')) {
+            const hashedName = await getHashedName(ReceiverAddress.replace(".sol", ""));
+            const nameAccountKey = await getNameAccountKey(
+              hashedName,
+              undefined,
+              new PublicKey("58PwtjSDuFHuUkYjH9BYnnQKHfwo9reZhC2zMJv9JPkx") // SOL TLD Authority
+            );
+            const owner = await NameRegistryState.retrieve(
+              connection,
+              nameAccountKey
+            );
+            destPubkey = owner.registry.owner;
+
+          }
+          // check if it is a twitter handle
+          else if (ReceiverAddress.includes('@')) {
+            const handle = ReceiverAddress.replace("@", "")
+            const registry = await getTwitterRegistry(connection, handle);
+            destPubkey = registry.owner;
+
+          }
+          else {
+            destPubkey = new PublicKey(ReceiverAddress);
+          }
+
+
+          let Tx = new Transaction()
+
+          for (let i = 0; i < Domains.length; i++) {
+
+            const ix = await transferNameOwnership(
+              connection,
+              Domains[i].replace(".sol", ""),
+              destPubkey,
+              undefined,
+              new PublicKey("58PwtjSDuFHuUkYjH9BYnnQKHfwo9reZhC2zMJv9JPkx") // SOL TLD Authority
+            );
+
+            Tx.add(ix)
+
+          }
+          //send the transaction
+          const sendSignature = await wallet.sendTransaction(Tx, connection);
+          // wait the confirmation
+          const confirmed = await connection.confirmTransaction(sendSignature, 'processed');
+
+
+          if (confirmed) {
+            const signature = sendSignature.toString();
+            setIsSending(false);
+            setSignature(signature)
+          }
+        }
+
+        catch (error) {
+          const err = (error as any)?.message;
+          setError(err);
+          setIsSending(false);
+        }
+      }
+
+    }
+  }
+
 
   return (
     <div className="container mx-auto max-w-6xl p-8 2xl:px-0">
@@ -635,7 +749,7 @@ export const MultiSenderView: FC = ({ }) => {
                 </h1>
                 <h3 className="font-semibold text-xl pb-5" >Supports public address, .sol domain name and Twitter handle with @</h3>
 
-                {nbToken == '' &&
+                {nbToken == '' && CurrencyType == '' &&
                   <div>
                     <div className="max-w-4xl mx-auto">
                       <ul className="text-left leading-10">
@@ -648,12 +762,20 @@ export const MultiSenderView: FC = ({ }) => {
                           </div>
                         </li>
 
-                        <li className="m-5" onClick={() => { setNbToken('multi'); reset()}}>
+                        <li className="m-5" onClick={() => { setNbToken('multi'); reset() }}>
                           <div className="p-4 hover:border">
                             <a className="text-4xl font-bold mb-5">
                               Multiple token - 1 receiver
                             </a>
                             <div>Send multiple tokens to one receiver</div>
+                          </div>
+                        </li>
+                        <li className="m-5" onClick={() => { setCurrencyType('domain'); reset() }}>
+                          <div className="p-4 hover:border">
+                            <a className="text-4xl font-bold mb-5">
+                              Domains transfer
+                            </a>
+                            <div>Transfer multiple Solana domains name to one receiver</div>
                           </div>
                         </li>
                       </ul>
@@ -1673,6 +1795,174 @@ export const MultiSenderView: FC = ({ }) => {
                     {signature != '' &&
                       <div className="font-semibold text-xl mt-4">
                         ✅ Successfuly sent! Check it <a target="_blank" href={'https://solscan.io/tx/' + signature}><strong className="underline">here</strong></a>
+                      </div>
+                    }
+
+                    {Error != '' && <div className="mt-4 font-semibold text-xl">❌ {Error}</div>}
+                  </div>}
+
+                {CurrencyType == 'domain' &&
+                  <div>
+
+                    <h1 className="font-bold mb-5 text-3xl uppercase">Domains sending</h1>
+                    <form className="mt-[3%] mb-[2%]">
+
+                      <input className="mb-[2%] md:w-[480px] text-center mx-4 text-black pl-1 border-2 border-black"
+                        type="text"
+                        required
+                        placeholder="Receiver Address"
+                        onChange={(e) => setReceiverAddress(e.target.value)}
+                        style={{
+                          borderRadius:
+                            "var(--rounded-btn,.5rem) var(--rounded-btn,.5rem)",
+                        }}
+                      />
+
+                      <div>
+                        <input className="mb-[1%] md:w-[480px] text-center mx-4 text-black pl-1 border-2 border-black"
+                          type="text"
+                          required
+                          placeholder=".sol domain name #1"
+                          onChange={(e) => setToken1(e.target.value)}
+                          style={{
+                            borderRadius:
+                              "var(--rounded-btn,.5rem) var(--rounded-btn,.5rem)",
+                          }}
+                        />
+                      </div>
+
+                      <div>
+                        <input className="mb-[1%] md:w-[480px] text-center mx-4 text-black pl-1 border-2 border-black"
+                          type="text"
+                          required
+                          placeholder=".sol domain name #2"
+                          onChange={(e) => setToken2(e.target.value)}
+                          style={{
+                            borderRadius:
+                              "var(--rounded-btn,.5rem) var(--rounded-btn,.5rem)",
+                          }}
+                        />
+                      </div>
+
+                      <div>
+                        <input className="mb-[1%] md:w-[480px] text-center mx-4 text-black pl-1 border-2 border-black"
+                          type="text"
+                          required
+                          placeholder=".sol domain name #3"
+                          onChange={(e) => setToken3(e.target.value)}
+                          style={{
+                            borderRadius:
+                              "var(--rounded-btn,.5rem) var(--rounded-btn,.5rem)",
+                          }}
+                        />
+                      </div>
+
+                      <div>
+                        <input className="mb-[1%] md:w-[480px] text-center mx-4 text-black pl-1 border-2 border-black"
+                          type="text"
+                          required
+                          placeholder=".sol domain name #4"
+                          onChange={(e) => setToken4(e.target.value)}
+                          style={{
+                            borderRadius:
+                              "var(--rounded-btn,.5rem) var(--rounded-btn,.5rem)",
+                          }}
+                        />
+                      </div>
+
+                      <div>
+                        <input className="mb-[1%] md:w-[480px] text-center mx-4 text-black pl-1 border-2 border-black"
+                          type="text"
+                          required
+                          placeholder=".sol domain name #5"
+                          onChange={(e) => setToken5(e.target.value)}
+                          style={{
+                            borderRadius:
+                              "var(--rounded-btn,.5rem) var(--rounded-btn,.5rem)",
+                          }}
+                        />
+                      </div>
+
+                      <div>
+                        <input className="mb-[1%] md:w-[480px] text-center mx-4 text-black pl-1 border-2 border-black"
+                          type="text"
+                          required
+                          placeholder=".sol domain name #6"
+                          onChange={(e) => setToken6(e.target.value)}
+                          style={{
+                            borderRadius:
+                              "var(--rounded-btn,.5rem) var(--rounded-btn,.5rem)",
+                          }}
+                        />
+                      </div>
+
+                      <div>
+                        <input className="mb-[1%] md:w-[480px] text-center mx-4 text-black pl-1 border-2 border-black"
+                          type="text"
+                          required
+                          placeholder=".sol domain name #7"
+                          onChange={(e) => setToken7(e.target.value)}
+                          style={{
+                            borderRadius:
+                              "var(--rounded-btn,.5rem) var(--rounded-btn,.5rem)",
+                          }}
+                        />
+                      </div>
+
+                      <div>
+                        <input className="mb-[1%] md:w-[480px] text-center mx-4 text-black pl-1 border-2 border-black"
+                          type="text"
+                          required
+                          placeholder=".sol domain name #8"
+                          onChange={(e) => setToken8(e.target.value)}
+                          style={{
+                            borderRadius:
+                              "var(--rounded-btn,.5rem) var(--rounded-btn,.5rem)",
+                          }}
+                        />
+                      </div>
+
+                      <div>
+                        <input className="sm:mb-[1%] mb-2 md:w-[480px] text-center mx-4 text-black pl-1 border-2 border-black"
+                          type="text"
+                          required
+                          placeholder=".sol domain name #9"
+                          onChange={(e) => setToken9(e.target.value)}
+                          style={{
+                            borderRadius:
+                              "var(--rounded-btn,.5rem) var(--rounded-btn,.5rem)",
+                          }}
+                        />
+                      </div>
+
+                      <div>
+                        <input className="sm:mb-[1%] mb-2 md:w-[480px] text-center mx-4 text-black pl-1 border-2 border-black"
+                          type="text"
+                          required
+                          placeholder=".sol domain name #10"
+                          onChange={(e) => setToken10(e.target.value)}
+                          style={{
+                            borderRadius:
+                              "var(--rounded-btn,.5rem) var(--rounded-btn,.5rem)",
+                          }}
+                        />
+                      </div>
+                    </form>
+
+                    {!isSending &&
+                      <button className="text-white font-semibold text-xl bg-[#414e63] hover:bg-[#2C3B52] w-[160px] rounded-full shadow-xl border" onClick={SendOnClickDomain}>Send</button>
+                    }
+                    {isSending &&
+                      <button className="text-white font-semibold text-xl bg-[#414e63] hover:bg-[#2C3B52] w-[160px] rounded-full shadow-xl border">
+                        <svg role="status" className="inline mr-3 w-4 h-4 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="#E5E7EB" />
+                          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentColor" />
+                        </svg>Sending</button>}
+
+
+                    {signature != '' &&
+                      <div className="font-semibold text-xl mt-4">
+                        ✅ Successfuly sent! Check it <a target="_blank" href={'https://solscan.io/tx/' + signature + '?cluster=devnet'}><strong className="underline">here</strong></a>
                       </div>
                     }
 
