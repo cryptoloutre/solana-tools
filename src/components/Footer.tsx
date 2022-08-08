b9dc9e123973b3bd2fbd273d05eff227a83e5088
cryptoloutre <miningcryptomonnaie@gmail.com>
2022-08-08 15:43:48 +0200

    change footer twitter link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
             <div className="w-100 pb-2">
                 <div className="flex justify-center">
 
-                    <a target="_blank" rel="noreferrer" href="https://twitter.com/laloutre" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-50 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
+                    <a target="_blank" rel="noreferrer" href="https://twitter.com/ToolsSolana" type="button" className="rounded-full border-2 border-white text-white leading-normal uppercase hover:bg-black hover:bg-opacity-50 focus:outline-none focus:ring-0 transition duration-150 ease-in-out w-9 h-9 m-1">
                         <svg aria-hidden="true"
                             focusable="false"
                             data-prefix="fab"
