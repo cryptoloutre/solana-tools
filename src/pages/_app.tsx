f664447892bded26e551854509c0c327ae0f85a6
cryptoloutre <miningcryptomonnaie@gmail.com>
2022-09-04 09:01:08 +0200

    change RPC (stop genesysgo fee RPC)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,9 @@ import "../styles/App.css";
 // set custom RPC server endpoint for the final website
 // const endpoint = "https://explorer-api.devnet.solana.com";
 // const endpoint = "http://127.0.0.1:8899";
- const endpoint = "https://ssc-dao.genesysgo.net";
+// const endpoint = "https://ssc-dao.genesysgo.net";
+const endpoint = "https://solana-api.projectserum.com";
+// const endpoint = "https://try-rpc.mainnet.solana.blockdaemon.tech";
 
 const WalletProvider = dynamic(
   () => import("../contexts/ClientWalletProvider"),
