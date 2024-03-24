549c0070df09f8c35de60e1bedac0f5266cb2246
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-03-24 19:02:45 +0800

    Update spltoken.tsx

--- src/pages/spltoken.tsx
+++ src/pages/spltoken.tsx
@@ -5,7 +5,7 @@ import Footer from "components/Footer";
 
 const Home: NextPage = (props) => {
   return (
-    <div className="flex flex-col h-screen justify-between">
+    <div className=" flex flex-col h-screen justify-between">
       <Head>
         <title>SOLANA TOOLS</title>
         <meta name="description" content="Solana tools to help you in the solana ecosystem" />
