4ad47e678ea04c4a4e64c00699b5377c53d882c3
AiPuZi <162868913+AiPuZi@users.noreply.github.com>
2024-04-01 13:54:15 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = (props) => {
   return (
     <div className=" flex flex-col h-screen justify-between">
       <Head>
-        <title>Create Solana token</title>
+        <title>Create Token</title>
         <meta name="description" content="Solana tools to help you in the solana ecosystem" />
       </Head>
       <SPLTokenView />
