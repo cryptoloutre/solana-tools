36f7b76c5fc2b130a341fadb87f58013476cfa2d
cryptoloutre <miningcryptomonnaie@gmail.com>
2024-11-19 14:02:51 +0100

    add description

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,29 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-        <link rel="shortcut icon" href="/favicon.ico"/>
+          <meta
+            name="description"
+            content="Bunch of free and open source tools to help you on Solana."
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="shortcut icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
